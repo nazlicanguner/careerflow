a536ca360275b4904be04a5b6086b1a09a157cfe
Nazlıcan Güner <nazlicanguner@gmail.com>
2026-08-26 14:51:58 +0200

    build: initialize Spring Boot backend

---
+++ careerflow-backend/src/main/java/com/nazlicanguner/careerflow/CareerflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nazlicanguner.careerflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerflowBackendApplication.class, args);
+	}
+
+}
---
+++ careerflow-backend/src/test/java/com/nazlicanguner/careerflow/CareerflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazlicanguner.careerflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
