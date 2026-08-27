23ab1f3006bfadcde768662c222d9f49938aff4d
Nazlıcan Güner <nazlicanguner@gmail.com>
2026-08-27 18:52:57 +0200

    feat: add company create and list API

---
+++ careerflow-backend/src/main/java/com/nazlicanguner/careerflow/company/CompanyController.java
@@ -0,0 +1,33 @@
+package com.nazlicanguner.careerflow.company;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/companies")
+public class CompanyController {
+
+    private final CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping
+    public List<Company> getAllCompanies() {
+        return companyService.getAllCompanies();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Company createCompany(@RequestBody Company company) {
+        return companyService.createCompany(company);
+    }
+}
---
+++ careerflow-backend/src/main/java/com/nazlicanguner/careerflow/company/CompanyService.java
@@ -0,0 +1,23 @@
+package com.nazlicanguner.careerflow.company;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyService {
+
+    private final CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    public Company createCompany(Company company) {
+        return companyRepository.save(company);
+    }
+}
